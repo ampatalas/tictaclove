d3909f6c46da3d1e97229c9301af980bac77c43a
ampatalas <a.m.patalas@gmail.com>
2015-02-11 23:12:09 +0100

    Initial commit

---
+++ app/src/main/java/com/ampatalas/tictaclove/GameplayActivity.java
@@ -0,0 +1,17 @@
+package com.ampatalas.tictaclove;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by A. M. Patalas on 2015-02-11.
+ */
+public class GameplayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+
+}
---
+++ app/src/main/java/com/ampatalas/tictaclove/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.ampatalas.tictaclove;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by A. M. Patalas on 2015-02-11.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
